4cfa2422b0655a8ba7bc4e1d684efd50751a1731
Florian Gandon <florian.gandon@ullink.com>
2016-05-11 14:52:14 +0300

    add editbinFolder property
    + unit tests

--- src/main/groovy/com/ullink/EditbinTask.groovy
+++ src/main/groovy/com/ullink/EditbinTask.groovy
@@ -6,6 +6,7 @@ import org.gradle.api.tasks.TaskAction
 public class EditbinTask extends DefaultTask {
     def targetFileName
     boolean enableLargeAddressAware
+    String editbinFolder
 
     @TaskAction
     def doIt() {
@@ -19,7 +20,7 @@ public class EditbinTask extends DefaultTask {
         def destination = project.file(targetFileName).getAbsolutePath()
         def editBinFile = findEditBin()
         if (editBinFile == null)
-            throw new GradleException("editbin file not found. Consider Installing Windows SDK or Visual C++ Build Tools")
+            throw new GradleException("editbin file not found: $editBinFile. Consider Installing Windows SDK or Visual C++ Build Tools or set editbinFolder property.")
 
         println "editbin Path: ${editBinFile}"
 
@@ -35,10 +36,17 @@ public class EditbinTask extends DefaultTask {
         println output
     }
 
-    File findEditBin(){
-        def programFiles = System.getenv("programfiles(x86)") ?: System.getenv("programfiles")
-        ['14.0', '12.0', '11.0', '10.0', '9.0', '8.0']
-                .collect { it -> new File("${programFiles}/Microsoft Visual Studio $it/VC/bin/editbin.exe") }
-                .find { it -> it.exists() }
+    String getProgramFiles() {
+        System.getenv("programfiles(x86)") ?: System.getenv("programfiles")
+    }
+
+    File findEditBin() {
+        if (editbinFolder) {
+            new File(editbinFolder)
+        } else {
+            ['14.0', '12.0', '11.0', '10.0', '9.0', '8.0']
+                    .collect { it -> new File("${programFiles}/Microsoft Visual Studio $it/VC/bin/editbin.exe") }
+                    .find { it -> it.exists() }
+        }
     }
 }
